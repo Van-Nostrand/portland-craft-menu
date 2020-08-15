3018d373ee275712b6f8c933566c4950f6afd737
Van-Nostrand <dmdoull43@gmail.com>
2020-08-15 15:02:27 -0700

    structured the header, minor styling

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,31 @@ import pclogo from "../assets/pclogo.png";
 class App extends React.Component{
 
   render(){
-    let mainStyle = {
-      backgroundImage: `url(${PCservice})`
-    }
+    
     return(
-      <div id="main-container" style={mainStyle}>
-        <nav>
-          <a href="#">HOME</a>
-          <a href="#">MENU</a>
-          <a href="#">RESTAURANT</a>
-          <span id="portland-logo">
-            <img src={pclogo} />
-          </span>
-          <a href="#">CONTACT</a>
-          <a href="#">EVENTS</a>
-        </nav>
-        <h2 id="tagline">
-          WE ARE OPEN FOR DINE IN AND TAKE OUT!
-        </h2>
+      <div id="landing-page">
+        <div id="header">
+          <div id="header-photo-wrapper">
+            <img id="restaurant-photo" src={PCservice} />
+          </div>
+          <nav>
+            <div id="nav-group-left">
+              <a href="#">HOME</a>
+              <a href="#">MENU</a>
+              <a href="#">RESTAURANT</a>
+            </div>
+            <span id="nav-group-logo">
+              <img src={pclogo} />
+            </span>
+            <div id="nav-group-right">
+              <a href="#">CONTACT</a>
+              <a href="#">EVENTS</a>
+            </div>
+          </nav>
+          <h2 id="header-tag">
+            WE ARE OPEN FOR DINE IN<br/>AND TAKE OUT!
+          </h2>
+        </div>
       </div>
     )
   }
