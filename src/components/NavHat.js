46d8e32b5d3c4b0e2048ae1856a4f6ee1262035d
Van-Nostrand <dmdoull43@gmail.com>
2020-08-30 09:31:51 -0700

    quick commit

--- src/components/NavHat.js
+++ src/components/NavHat.js
@@ -1,11 +1,40 @@
 import React from "react";
 import pclogo from "../assets/pclogo.png";
+import pcMezzanine from "../assets/PCservice.png";
 //TODO: rename nav-hat-wrapper to something like page-wrapper
 export const NavHat = ({changePage}) => {
 
 
   return(
     <div className="nav-hat-wrapper">
+
+      {/* <div className="background-photo-wrapper">
+        <img id="restaurant-photo" src={pcMezzanine} />
+      </div> */}
+    
+      {/* <nav className="full-nav" >
+
+        <div className="full-nav__group-left">
+          <a href="#">
+            HOME
+          </a>
+          <a href="#">
+            MENU
+          </a>
+          
+          <a href="#">RESTAURANT</a>
+        </div>
+
+        <span className="full-nav__group-logo">
+          <img src={pclogo} />
+        </span>
+
+        <div className="full-nav__group-right">
+          <a href="#">CONTACT</a>
+          <a href="#">EVENTS</a>
+        </div>
+
+      </nav> */}
       
       <nav className="mobile-nav" >
 
