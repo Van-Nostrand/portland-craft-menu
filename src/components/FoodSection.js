454b33741c864fef862b12098f6848e4378baef1
Van-Nostrand <dmdoull43@gmail.com>
2020-08-30 12:26:59 -0700

    restyling desktop menu borders

--- src/components/FoodSection.js
+++ src/components/FoodSection.js
@@ -16,19 +16,22 @@ export const FoodSection = ({section, items, logos}) => {
               logos={dietlogos} 
               key={`${section}-item-${i}`} />
   });
+            
+  let sectionClass = `food-section ${section}`;
 
   if(section === "burgers"){
     section = (
       <>
         {section}
-        <div className="burgers">All burgers are served on a brioche bun<br /> and come with a side of fries <br />(Sub salad or half-&-half for $2)</div>
+        <div>All burgers are served on a brioche bun<br /> and come with a side of fries <br />(Sub salad or half-&-half for $2)</div>
       </>
     )
   }
 
+
   // TODO: rename class menu-item-wrapper to something better
   return(
-    <section className="food-section">
+    <section className={sectionClass}>
       <div className="section__title">{section}</div>
       <ul className="section__list">
         {foods}
