8c490d8447bd29b306de79bc4313e3ed7586844b
Van-Nostrand <dmdoull43@gmail.com>
2020-09-19 16:07:27 -0700

    new updated constants sheet

---
+++ src/components/CONSTANTS2.js
@@ -0,0 +1,534 @@
+export const SNACKS, LARGER, SWEETS
+
+export const SNACKS = [
+  {
+    name: "Pickled Vegetables",
+    price: "6",
+    notes: "House-pickled seasonally sourced selections",
+    vegetarian: false,
+    vegan: true,
+    glutenFree: true, 
+    nutFree: true, 
+    dairyFree: true
+  },
+  {
+    name: "Felix Farm Kennebec Fries",
+    price: "7",
+    notes: "Double-cooked Felix Farm Kennebec potatoes, black garlic aioli, fresh herbs",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: true, 
+    nutFree: true, 
+    dairyFree: false
+  },
+  {
+    name: "Warm Mixed Olives",
+    price: "7",
+    // notes: "House marinated with citrus and herb",
+    notes: "House marinated with citrus and herbs",
+    vegetarian: false,
+    vegan: true,
+    glutenFree: true, 
+    nutFree: true, 
+    dairyFree: true
+  },
+  {
+    name: "Spiced Candied Nuts",
+    price: "7",
+    // notes: "Roasted in house with our spice blend",
+    notes: "Roasted in-house with our own spice blend",
+    vegetarian: false,
+    vegan: true,
+    glutenFree: true, 
+    nutFree: false, 
+    dairyFree: true
+  },
+  {
+    name: "Crispy Rossdown Chicken Crackling",
+    price: "8",
+    // notes: "Fried chicken skin with house seasoning blend of lime, chilis, lemongrass, and herbs",
+    notes: "Fried chicken skin seasoned with a blend of lime, chilis, lemongrass, herbs",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: true, 
+    nutFree: true, 
+    dairyFree: true
+  },
+  {
+    name: "Cashew Spread",
+    price: "10",
+    // Maybe we could use less of the word "house"?
+    // notes: "House-made cashew spread, house-pickled vegetables, fresh herbs, house crisps/bread",
+    notes: "With pickled vegetables, fresh herbs, house-baked bread",
+    vegetarian: false,
+    vegan: true,
+    glutenFree: false, 
+    nutFree: false, 
+    dairyFree: true
+  },
+  {
+    name: "Candied Bacon",
+    price: ["2", "10"],
+    notes: "Johnston Farm smoked bacon baked with house-spice blend",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: true, 
+    nutFree: true, 
+    dairyFree: true
+  },
+  {
+    name: "Craft Poutine",
+    price: "11",
+    // plural "curds"
+    // notes: "Double-cooked Felix Farm Kennebec potatoes, Quebec cheese curd, house-made chicken gravy",
+    notes: "Double-cooked Felix Farm Kennebec potatoes, Quebec cheese curds, house-made chicken gravy",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: false, 
+    nutFree: true, 
+    dairyFree: false
+  }
+];
+
+export const SMALL_PLATES = [
+  {
+    subSection: "Omnivores",
+    items: [
+      {
+        name: "Rossdown Wings",
+        price: "18",
+        // notes: "1 lb wings, daikon, carrots, celery, garlic chili butter sauce",
+        notes: "1lb of wings, daikon, carrots, celery, garlic chili butter sauce",
+        vegetarian: false,
+        vegan: false,
+        glutenFree: true, 
+        nutFree: false, 
+        dairyFree: false
+      },
+      {
+        name: "Pacific Humboldt Squid",
+        price: "12",
+        // capital
+        // notes: "tomato, leeks, garlic, chilis, herbs",
+        notes: "Tomato, leeks, garlic, chilis, herbs",
+        vegetarian: false,
+        vegan: false,
+        glutenFree: true, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        name: "Pacific Wild Shrimp Fritter",
+        price: "10",
+        // erraneous "and"
+        // notes: "Corn, soy bean, herbs, and spicy black garlic aioli",
+        notes: "Corn, soy bean, herbs, spicy black garlic aioli",
+        vegetarian: false,
+        vegan: false,
+        glutenFree: false, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        // asterisk for no reason? There's no footnote anywhere
+        // name: "Westcoast* Crab Dip",
+        name: "Westcoast Crab Dip",
+        price: "15",
+        // capital
+        // notes: "bacon, leeks, herbs, house bread",
+        notes: "Bacon, leeks, herbs, house bread",
+        vegetarian: false,
+        vegan: false,
+        glutenFree: false, 
+        nutFree: true, 
+        dairyFree: false
+      },
+      {
+        name: "Fraser Valley Pork Belly",
+        price: "13",
+        // seared belly vs seared pork belly? 
+        // "smoke onion" sounds like an instruction. is it supposed to be "smoked onion jam"?
+        // notes: "Seared belly, house pickles, green onions, smoke onion, jam",
+        notes: "Seared pork belly, house pickles, green onions, smoked onion jam",
+        vegetarian: false,
+        vegan: false,
+        glutenFree: true, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        name: "Cache Creek Beef Tartar",
+        price: "18",
+        notes: "Black garlic, celery, radish, egg yolk, shallots, toast",
+        vegetarian: false,
+        vegan: false,
+        glutenFree: false, 
+        nutFree: true, 
+        dairyFree: true
+      },
+    ]
+  },
+  {
+    subSection: "Herbivores",
+    items: [
+      {
+        name: "B.C. Kale Salad",
+        price: "9",
+        notes: "Tomato, radish, pumpkin seeds, balsamic soy dressing",
+        vegetarian: true,
+        vegan: true,
+        glutenFree: true, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        // The colloquial term is "corn-on-the-cob", not "a cob", would we rather go with this?
+        // name: "Chilliwack Corn on a Cob",
+        name: "Chilliwack Corn-on-the-Cob",
+        price: "7",
+        notes: "Grilled corn, chili lime butter, citrus herb crumble",
+        vegetarian: true,
+        vegan: false,
+        glutenFree: false, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        name: "Heirloom Tomato Salad",
+        price: "11",
+        notes: "Avocado, basil, radish, balsamic aioli, sourdough",
+        vegetarian: true,
+        vegan: false,
+        glutenFree: false, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        name: '"Fried Rice"',
+        price: "14",
+        // notes: "Mushrooms, gai lan, corn, shallots sesame sauce",
+        notes: "Mushrooms, gai lan, corn, shallots, sesame sauce",
+        vegetarian: true,
+        vegan: true,
+        glutenFree: true, 
+        nutFree: true, 
+        dairyFree: true
+      },
+      {
+        // This name needs reworking... can Gai Lan, broccoli, and kale go in the description?
+        name: "B.C. Brasica (Gai Lan/Broccoli/Kale)",
+        price: "8",
+        notes: "Cashews, Garlic, herbs",
+        vegetarian: true,
+        vegan: true,
+        glutenFree: true, 
+        nutFree: false, 
+        dairyFree: true
+      },
+    ]
+  }
+];
+
+export const LARGER = [
+  {
+    name: "Cache Creek Dry-Aged Burger",
+    price: "20",
+    notes: "Cheddar, bacon, onion, lettuce, tomato, daikon, aioli",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: false, 
+    nutFree: true, 
+    dairyFree: false,
+  },
+  {
+    name: "Rossdown Chicken and Waffle",
+    price: "23",
+    notes: "Fried chicken breast, mushroom, bacon, butter, coconut gravy",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: false, 
+    nutFree: true, 
+    dairyFree: false
+  },
+  {
+    name: "Pacific Wild Salmon",
+    price: "24",
+    notes: "Fried rice, corn, broccoli, sesame leek sauce",
+    vegetarian: false,
+    vegan: false,
+    glutenFree: true, 
+    nutFree: true, 
+    dairyFree: true
+  },
+  {
+    name: "Grain Bowl",
+    price: "21",
+    notes: "Farro, organic greens, tomato, cucumber, soy bean, tofu, cashew herb dressing",
+    vegetarian: true,
+    vegan: true,
+    glutenFree: true, 
+    nutFree: false, 
+    dairyFree: true
+  },
+  {
+    name: "Biscuit and Gravy",
+    price: "18",
+    notes: "House-made lemon herb biscuit, honey butter, braised greens, coconut gravy",
+    vegetarian: true,
+    vegan: false,
+    glutenFree: false, 
+    nutFree: true, 
+    dairyFree: false
+  }
+];
+
+export const SWEETS = [
+  {
+    name: "Chocolate Mousse",
+    price: "9", 
+    // spelling
+    // notes: "Sponge cake, whipped cream, cocao nibs",
+    notes: "Sponge cake, whipped cream, cacao nibs",
+    vegetarian: true,
+    vegan: false,
+    glutenFree: false, 
+    nutFree: true, 
+    dairyFree: false
+  },
+  {
+    // "Stone fruit fried" sounds like the pie was fried in stone fruits, or using a method involving stone fruits. 
+    // name: "Stone Fruit Fried Pie",
+    name: "Fried Stone Fruit Pie",
+    price: "8",
+    notes: "Sponge cake, whipped cream, cacao nibs",
+    vegetarian: true,
+    vegan: false,
+    glutenFree: false, 
+    nutFree: false, 
+    dairyFree: false
+  },
+]
+
+
+// =========
+// BEVERAGES
+// =========
+
+export const WHITE_WINE = [
+  {
+    name: "Tightrope",
+    varietal: "Pinot Gris",
+    sizes: [10, 14, 42],
+    notes: "BC"
+  },
+  {
+    name: "Sandhill",
+    varietal: "Pinot Blanc",
+    sizes: [11, 15, 45],
+    notes: "BC"
+  },
+  {
+    name: "Gray Monk",
+    varietal: "Chardonnay",
+    sizes: [12, 16, 48],
+    notes: "BC"
+  },
+  {
+    name: "Bonamici",
+    varietal: "Rose",
+    sizes: [10, 14, 42],
+    notes: "BC"
+  },
+  {
+    name: "Kanazawa",
+    varietal: "Nomu",
+    sizes: [12, 16, 48],
+    notes: "BC"
+  },
+  {
+    name: "Charles Smith",
+    varietal: "Riesling",
+    sizes: [12, 16, 48],
+    notes: "Washington"
+  },
+  {
+    name: "Kim Crawford",
+    varietal: "Sauvignon Blanc",
+    sizes: [12, 16, 48],
+    notes: "New Zealand"
+  }
+];
+
+export const RED_WINE = [
+  {
+    name: "Lake Breeze",
+    varietal: "Meritage",
+    sizes: [12, 16, 48],
+    notes: "BC"
+  },
+  {
+    name: "Septima",
+    varietal: "Malbec",
+    sizes: [10, 14, 42],
+    notes: "Argentina"
+  },
+  {
+    name: "James Mitchell",
+    varietal: "Cabernet Sauvignon",
+    sizes: [12, 16, 48],
+    notes: "California"
+  },
+  {
+    name: "Sandhill",
+    varietal: "Cabernet Merlot",
+    sizes: [11, 15, 45],
+    notes: "BC"
+  },
+  {
+    name: "See Ya Later Ranch",
+    varietal: "Pinot Noir",
+    sizes: [11, 15, 45],
+    notes: "BC"
+  },
+  {
+    name: "Kanazawa",
+    varietal: "Merlot",
+    sizes: [12, 16, 48],
+    notes: "BC"
+  }
+]
+
+export const BUBBLES = [
+  {
+    name: "Stellars Jay",
+    sizes: [11, 45],
+    notes: "BC"
+  },
+  {
+    name: "Codorniu Cava",
+    sizes: [10, 42],
+    notes: "Spain"
+  }
+]
+
+export const COCKTAILS = [
+  {
+    name: "Spritz Veneziano",
+    price: "11",
+    notes: ["Ruffino Prosecco, soda, choice of liqueur:", ["Aperol", "Campari", "Elderflower & Cassis ($12)"]]
+  },
+  {
+    name: "Margarita",
+    price: "12",
+    notes: "El Jimador Blanco tequila, lime, agave"
+  },
+  {
+    name: "Moscow Mule / Dark & Stormy",
+    price: "12",
+    notes: "Absolut vodka or Goslings black rum, sugar, Angostura bitters, Fentiman's ginger beer"
+  },
+  {
+    name: "Whiskey Sour",
+    price: "13",
+    notes: "Wild Turkey bourbon, lemon, sugar, Angostura bitters, egg white"
+  },
+  {
+    name: "Corpse Reviver #2",
+    price: "12",
+    notes: "Beefeater gin, Cointreau, Lillet Blanc, Pernod rinse, lemon"
+  },
+  {
+    name: "Daquiri",
+    price: "12",
+    notes: "dark rum, lime, sugar"
+  },
+  {
+    name: "Negroni Sour",
+    price: "13",
+    notes: "Beefeater gin, Campari, Odd Society Amaro, orange, lemon, sugar, egg white. Cory's own creation!"
+  },
+  {
+    name: "Best Bloody Caesar",
+    price: "9 / 13",
+    notes: "Absolut vodka, house caesar mix, Clamato, pickled veg. Can be ordered as a single or double"
+  },
+  {
+    name: "Fraser Collins",
+    price: "13",
+    notes: "Beefeater gin, lemon, sugar, cucumber, black pepper, topped off with pilsner. Mike's twist on the Tom Collins!"
+  },
+  {
+    name: "Spanish Fashion",
+    price: "13",
+    notes: "Cazadores A\u00f1ejo tequila, Cinzano Rosso, Angostura bitters"
+  }
+]
+
+
+
+export const HAPPY_HOUR = [
+  {
+    name: "Select US Craft Beer",
+    prices: [
+      {price: "5.75", size: "(14oz)"}
+    ]
+  },
+  {
+    name: "Select Local Draught Beer",
+    prices: [
+      {price: "4", size: "(12oz)"},
+      {price: "5", size: "(20oz)"}
+    ]
+  },
+  {
+    name: "House Red, White, & Rose Wine",
+    prices: [
+      {price: "5.50", size: "(5oz)"},
+      {price: "9", size: "(8oz)"}
+    ]
+  },
+  {
+    name: "Aperol Spritz & Moscow Mule",
+    prices: [
+      {price: "9", size: ""},
+    ]
+  },
+  {
+    name: "Select Single/Double Highballs",
+    prices: [
+      {price: "5/7.50", size: ""}
+    ]
+  },
+  {
+    name: "Jameson",
+    prices: [
+      {price: "4.50", size: "(1oz)"}
+    ]
+  }
+]
+
+export const NON_ALCOHOLIC = [
+  {
+    name: "Pop",
+    price: "3.50",
+    notes: "Pepsi, Diet Pepsi, Sprite, Ginger Ale, Soda, Tonic"
+  },
+  {
+    name: "Orange Juice",
+    price: "3.50"
+  },
+  {
+    name: "Cranberry Juice",
+    price: "3.50"
+  },
+  {
+    name: "Fentimans Sodas",
+    price: "5",
+    notes: "Ginger beer or Burdock and Dandelion"
+  },
+  {
+    name: "Mocktails",
+    price: "5",
+    notes: "PLACEHOLDER"
+  }
+];
