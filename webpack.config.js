10872208dd29a4f9d572c0dedab360d331381fee
Van-Nostrand <dmdoull43@gmail.com>
2020-11-10 14:08:17 -0800

    converted back to light theme, added Raleway font

--- webpack.config.js
+++ webpack.config.js
@@ -41,9 +41,6 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {
-
-            }
           },
           "css-loader"
         ]
@@ -63,12 +60,15 @@ module.exports = {
       {
         test: /\.(ttf|eot|woff|woff2)$/,
         exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          name: "[name].[ext]",
-          outputPath: 'fonts/'
-        }    
-        
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[ext]",
+              outputPath: 'fonts/'
+            }
+          }
+        ] 
       }
     ]
   },
