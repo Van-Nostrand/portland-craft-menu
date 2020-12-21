1aa55c6f06a391669ac96fd15cc550393bbe7897
Van-Nostrand <dmdoull43@gmail.com>
2020-12-20 17:59:44 -0800

    added and updated most of the spirits

--- src/components/PageLoader.js
+++ src/components/PageLoader.js
@@ -14,7 +14,9 @@ import {
   GIN,
   VODKA,
   TEQUILA,
+  RUM,
   OTHER_SPIRITS,
+  BRANDY,
   HAPPY_HOUR,
   HAPPY_HOUR_FOOD,
   FEATURES,
@@ -35,7 +37,7 @@ export const PageLoader = ({ pageTitle}) => {
       currentPage = <FoodMenu menuData={{SNACKS,OMNIVORES,HERBIVORES,LARGER,SWEETS,SIDES,FEATURES}} />;
       break;
     case /drinks/.test(pageTitle): 
-      currentPage = <DrinksMenu menuData={{WHITE_WINE, RED_WINE, BUBBLES, HOUSE_COCKTAILS, BOURBON, RYE, SCOTCH, OTHER_WHISKEY, GIN, VODKA, TEQUILA, OTHER_SPIRITS, PACKAGED_BEER}} />;
+      currentPage = <DrinksMenu menuData={{WHITE_WINE, RED_WINE, BUBBLES, HOUSE_COCKTAILS, BOURBON, RYE, SCOTCH, OTHER_WHISKEY, GIN, VODKA, TEQUILA, OTHER_SPIRITS, RUM, BRANDY, PACKAGED_BEER}} />;
       break;
     case /specials/.test(pageTitle): 
       currentPage = <HappyHour menuData={[HAPPY_HOUR, HAPPY_HOUR_FOOD]} />;
