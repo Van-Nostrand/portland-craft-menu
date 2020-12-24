d7f5f52ae8a392cf89eb86f96bba1234472d495b
Van-Nostrand <dmdoull43@gmail.com>
2020-12-24 00:29:56 -0800

    began writing api

---
+++ src/service/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const ROOT_URL = "http://localhost:8081";
+
+export function apiCall(method, path, data){
+  return new Promise((resolve, reject) => {
+    return axios({
+      method: method.toLowerCase(),
+      url: `${ROOT_URL}${path}`,
+      data: {...data}
+    })
+      .then(res => {
+        return resolve(res.data)
+      }).catch(err => {
+        return reject(err.response.data.error);
+      });
+  })
+}
